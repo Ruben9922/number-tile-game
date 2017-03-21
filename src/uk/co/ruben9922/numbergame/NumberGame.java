be80d1489fd003ff2195f076c3f32d91f62bc527
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-03-21 22:41:11 +0000

    Add min tile number parameter

--- src/uk/co/ruben9922/numbergame/NumberGame.java
+++ src/uk/co/ruben9922/numbergame/NumberGame.java
@@ -23,7 +23,7 @@ public class NumberGame {
             System.out.println(player.getName());
         }
 
-        List<Tile> tiles = generateTiles(2, 13, Arrays.asList(new Tile[] {
+        List<Tile> tiles = generateTiles(2, 1, 13, Arrays.asList(new Tile[] {
                 new SmileyTile(Tile.Colour.BLACK),
                 new SmileyTile(Tile.Colour.ORANGE)
         }));
@@ -74,14 +74,14 @@ public class NumberGame {
         return Arrays.asList(playerArray);
     }
 
-    // maxTileNumber is inclusive
-    private static List<Tile> generateTiles(int numberTileCopies, int maxTileNumber, List<Tile> extraTiles) {
+    // minTileNumber and maxTileNumber are both inclusive
+    private static List<Tile> generateTiles(int numberTileCopies, int minTileNumber, int maxTileNumber, List<Tile> extraTiles) {
         Tile.Colour[] colourValues = Tile.Colour.values();
         List<Tile> tiles = new ArrayList<>((numberTileCopies * colourValues.length) + extraTiles.size());
 
         for (Tile.Colour colour : colourValues) {
             for (int i = 0; i < numberTileCopies; i++) {
-                for (int j = 1; j <= maxTileNumber; j++) {
+                for (int j = minTileNumber; j <= maxTileNumber; j++) {
                     tiles.add(new NumberTile(colour, j));
                 }
             }
