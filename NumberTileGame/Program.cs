163de1892bbaa8c9eadc44b3023daca3ef5b70d7
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-05-01 17:47:29 +0100

    Input player names

--- NumberTileGame/Program.cs
+++ NumberTileGame/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace NumberTileGame
+﻿using System;
+
+namespace NumberTileGame
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            const int playerCount = 2;
+            Player[] players = new Player[playerCount];
+
+            for (int i = 0; i < playerCount; i++)
+            {
+                Console.WriteLine($"Name of Player {i + 1} [Player {i + 1}]: ");
+                string playerName = Console.ReadLine();
+                if (playerName == "")
+                {
+                    playerName = $"Player {i + 1}";
+                }
+
+                players[i] = new Player(playerName);
+            }
+        }
+    }
+
+    internal class Player
+    {
+        private string PlayerName { get; set; }
+
+        public Player(string playerName)
+        {
+            PlayerName = playerName;
         }
     }
 }
