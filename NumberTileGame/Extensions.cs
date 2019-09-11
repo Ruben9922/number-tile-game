3345ab45f8fb2ea7191f25a0bf95fa5a83a50be3
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-09-11 17:30:32 +0100

    Print "<None>" if collection empty

--- NumberTileGame/Extensions.cs
+++ NumberTileGame/Extensions.cs
@@ -1,21 +1,27 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace NumberTileGame
 {
     public static class Extensions
     {
-        public static string ToHumanReadableString<T>(this IEnumerable<T> enumerable)
+        public static string ToHumanReadableString<T>(this ICollection<T> collection)
         {
-            StringBuilder stringBuilder = new StringBuilder();
+            if (collection.Count == 0)
+            {
+                return "<None>";
+            }
             
+            StringBuilder stringBuilder = new StringBuilder();
+
             int i = 0;
-            foreach (T item in enumerable)
+            foreach (T item in collection)
             {
                 stringBuilder.AppendLine($"[{i}]: {item}");
                 i++;
             }
-            
+
             return stringBuilder.ToString();
         }
     }
